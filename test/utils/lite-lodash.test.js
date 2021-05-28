fef78c1f8c32bf223a2af5aae91c94356227f63e
孟陬 <chuanzong.lcz@antgroup.com>
2021-05-28 12:33:34 +0800

    test: not add coverage into gitignore when it includes xxxcoveragexx

--- test/utils/lite-lodash.test.js
+++ test/utils/lite-lodash.test.js
@@ -1,4 +1,4 @@
-const { intersection } = require('../../src/utils/lite-lodash');
+const { intersection, hasWord } = require('../../src/utils/lite-lodash');
 
 describe('lite-lodash', () => {
   describe('intersection', () => {
@@ -15,6 +15,43 @@ describe('lite-lodash', () => {
       const arr2 = ['@type/jest'];
       const actual = intersection(arr1, arr2);
       const expected = [];
+
+      expect(actual).toEqual(expected);
+    });
+  });
+
+  describe('hasWord', () => {
+    it('Should has word "coverage" in the end', () => {
+      const actual = hasWord('.mini ide coverage', 'coverage');
+      const expected = true;
+
+      expect(actual).toEqual(expected);
+    });
+
+    it('has word "coverage" in the middle', () => {
+      const actual = hasWord('.mini ide coverage middle', 'coverage');
+      const expected = true;
+
+      expect(actual).toEqual(expected);
+    });
+
+    it('"coverage2" not has word "coverage"', () => {
+      const actual = hasWord('.mini ide coverage2', 'coverage');
+      const expected = false;
+
+      expect(actual).toEqual(expected);
+    });
+
+    it('"idecoverage/" not has word "coverage"', () => {
+      const actual = hasWord('idecoverage/', 'coverage');
+      const expected = false;
+
+      expect(actual).toEqual(expected);
+    });
+
+    it('"idecoverage" not has word "coverage"', () => {
+      const actual = hasWord('idecoverage', 'coverage');
+      const expected = false;
 
       expect(actual).toEqual(expected);
     });
